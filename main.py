ae3d79de9b2a3375b426089159b663852afe7746
Farhan Fahrezy <mohdfarhanfahrezy@gmail.com>
2022-11-25 15:35:57 +0700

    cnf converter to main

---
+++ main.py
@@ -0,0 +1,33 @@
+import CFGtoCNF
+import codeTokenizer as ctok
+import cyk
+import sys
+
+# # Get CNF Grammar
+# CFGtoCNF.Converter()
+
+# Load Chomsky Normal Form
+if len(sys.argv) > 1:
+    inputJs = str(sys.argv[1])
+else:
+	print("Must be 2 arument!")
+
+
+cyk.LoadCNF("./Grammar/CNF.txt")
+
+# Tokenize
+tokenizedCode = ctok.tokenizeInput(inputJs)
+print(tokenizedCode)
+
+# CYK
+
+table = cyk.cyk(tokenizedCode)
+
+print("========================================================")
+for x in table:
+    print(x)
+
+if (cyk.checkValidity(table, "S")):
+    print("Verdict accepted! Compile success!")
+else:
+    print("Compile error, wrong syntax!")
